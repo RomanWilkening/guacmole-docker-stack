82be1ea8fe5a00fee67691ed3d3c01790f0c585c
RomanWilkening <rwilkening@online.de>
2021-02-22 17:40:03 +0100

    Update initdb.sql

--- init/initdb.sql
+++ init/initdb.sql
@@ -1,26 +1,3 @@
---
--- Licensed to the Apache Software Foundation (ASF) under one
--- or more contributor license agreements.  See the NOTICE file
--- distributed with this work for additional information
--- regarding copyright ownership.  The ASF licenses this file
--- to you under the Apache License, Version 2.0 (the
--- "License"); you may not use this file except in compliance
--- with the License.  You may obtain a copy of the License at
---
---   http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing,
--- software distributed under the License is distributed on an
--- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
--- KIND, either express or implied.  See the License for the
--- specific language governing permissions and limitations
--- under the License.
---
-
---
--- Connection group types
---
-
 CREATE TYPE guacamole_connection_group_type AS ENUM(
     'ORGANIZATIONAL',
     'BALANCING'
@@ -733,25 +710,6 @@ CREATE TABLE guacamole_user_password_history (
 
 CREATE INDEX guacamole_user_password_history_user_id
     ON guacamole_user_password_history(user_id);
-
---
--- Licensed to the Apache Software Foundation (ASF) under one
--- or more contributor license agreements.  See the NOTICE file
--- distributed with this work for additional information
--- regarding copyright ownership.  The ASF licenses this file
--- to you under the Apache License, Version 2.0 (the
--- "License"); you may not use this file except in compliance
--- with the License.  You may obtain a copy of the License at
---
---   http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing,
--- software distributed under the License is distributed on an
--- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
--- KIND, either express or implied.  See the License for the
--- specific language governing permissions and limitations
--- under the License.
---
 
 -- Create default user "guacadmin" with password "guacadmin"
 INSERT INTO guacamole_entity (name, type) VALUES ('guacadmin', 'USER');
